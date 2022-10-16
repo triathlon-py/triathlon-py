ff0ecb7d8e3beef0ce2201b5ae763c46a9cdbe30
0xMRTT <0xMRTT@tuta.io>
2022-10-16 16:32:56 +0200

    feat: add courses api

--- triathlon/triathlon.py
+++ triathlon/triathlon.py
@@ -97,7 +97,7 @@ class SearchEndpoint(BaseEndpoint):
         return f"{self.url}/{self.collection}"
 
     def req_body(self, page=None, per_page=None, query=None, filters=None, location=None, distance=None, start_date=None, end_date=None, elite=False):
-        return {"page": page, "per_page": per_page, "query": query, "filters": filters, "location":location, "distance":distance, "start_date": start_date, "end_date":end_date, "elite":elite}, None
+        return {"page": page, "per_page": per_page, "query": query, "filters": filters, "location": location, "distance": distance, "start_date": start_date, "end_date": end_date, "elite": elite}, None
 
 
 class AtheletesSearchEndpoint(SearchEndpoint):
@@ -127,34 +127,71 @@ class VideosSearchEndpoint(SearchEndpoint):
 class AthleteEndpoint(BaseEndpoint):
     endpoint = "athlete"
 
+
+class CoursesEndpoint(BaseEndpoint):
+    endpoint = "courses"
+
+    def req_body(self, page=None, per_page=None, category_id=None, name=None, country_id=None, region_id=None, start_date=None, end_date=None, language=None, order=None, course_id=None):
+        return {"page": page, "per_page": per_page, "category_id": category_id, "name": name, "country_id": country_id, "region_id": region_id, "start_date": start_date, "end_date": end_date, "language": language, "order": order, "course_id": course_id}, None
+
+class CoursesInfoEndpoint(CoursesEndpoint):
+    def resolve_url(self, **kwargs):
+        return f"{self.url}/{kwargs['course_id']}"
+    
+    def req_body(self, course_id=None):
+        return None, None
+    
+class CoursesCategoriesEndpoint(CoursesEndpoint):
+    
+    def resolve_url(self, **kwargs):
+        return f"{self.url}/categories"
+    
+    def req_body(self,cat_parent_id=None, child_categories=False ):
+        return {"cat_parent_id": cat_parent_id, "child_categories":child_categories}, None
+    
 class SearchAPI:
     def __init__(self, token) -> None:
         self.token = token
+
     def athletes(self, query, **kwargs):
         return AtheletesSearchEndpoint(self.token, query=query, **kwargs).request()
-    
+
     def events(self, query, **kwargs):
         return EventsSearchEndpoint(self.token, query=query, **kwargs).request()
-    
+
     def federations(self, query, **kwargs):
         return FederationsSearchEndpoint(self.token, query=query, **kwargs).request()
-        
+
     def courses(self, query, **kwargs):
         return CoursesSearchEndpoint(self.token, query=query, **kwargs).request()
-    
+
     def news(self, query, **kwargs):
         return NewsSearchEndpoint(self.token, query=query, **kwargs).request()
-    
+
     def videos(self, query, **kwargs):
         return VideosSearchEndpoint(self.token, query=query, **kwargs).request()
 
+class CoursesAPI:
+    def __init__(self, token) -> None:
+        self.token = token
+        
+    def search(self, **kwargs):
+        return CoursesEndpoint(self.token, **kwargs).request()
+    
+    def info(self, course_id):
+        return CoursesInfoEndpoint(self.token, course_id=course_id).request()
+    
+    def categories(self, **kwargs):
+        return CoursesCategoriesEndpoint(self.token, **kwargs).request()
 class TriathlonAPI:
     def __init__(self, token) -> None:
         self.token = token
         self.search = SearchAPI(token)
-        
+        self.courses = CoursesAPI(token)
+    
+
 if __name__ == "__main__":
     with open("../token.txt", "r", encoding="utf-8") as f:
         token = f.read()
     api = TriathlonAPI(token)
-    print(api.search.events("London"))
+    print(api.courses.info(78443))
